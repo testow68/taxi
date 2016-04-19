fe4a29caa96de81327f202f990fdd39880989b5c
Przemysław Piwowarski <prz.piw@mail.com>
2016-04-19 21:15:50 +0200

    first c

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">     
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> 
+  <link rel="stylesheet" href="secondstep.css" type="text/css" />
+<link href='/SecondStep/Oswald-Regular.ttf' rel='stylesheet' type='text/css'>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ciasteczka.eu/cookiesEU-latest.min.js"></script>
+    </head>
+    <body>
+        
+          <div id="img1" class="parallax-bg">
+    <h1>You talkin' to me?</h1>
+  </div>
+        <header>
+           The idea had been growing in my brain <br>
+           for some time...
+        </header>
+  <section>
+  Ladies and gentlemen… we are met today at a crossroads, Columbus Circle. This is no ordinary place. It is a place where many roads and many lives intersect. It is appropriate that we meet here today, because these are not ordinary times. We meet at a crossroads in history. For far too long, the wrong roads have been taken. The wrong roads have led us into war, into poverty, into unemployment, and inflation. Today I say to you, we have reached the turning point. No longer will we, the people, suffer for the few. I would lie to you if I told you the new roads would be easy. They will not be easy. Nothing that is right and good has ever been easy. We, the people, know that. And we, the people, know the right roads and the good. Today I say to you, we are the people, you and I. And it is time to let the people rule! Thank you. (ang.)
+	Postać: senator Charles Palantine
+
+  </section>
+      
+      <div id="img2" class="parallax-bg">
+    <h1>Then who the hell else are you talking to?</h1>
+  </div>
+        
+         <header>
+           Thank God for the rain, which washed away <br> the garbage and the trash off the sidewalks.
+        </header>
+  <section>
+  Ladies and gentlemen… we are met today at a crossroads, Columbus Circle. This is no ordinary place. It is a place where many roads and many lives intersect. It is appropriate that we meet here today, because these are not ordinary times. We meet at a crossroads in history. For far too long, the wrong roads have been taken. The wrong roads have led us into war, into poverty, into unemployment, and inflation. Today I say to you, we have reached the turning point. No longer will we, the people, suffer for the few. I would lie to you if I told you the new roads would be easy. They will not be easy. Nothing that is right and good has ever been easy. We, the people, know that. And we, the people, know the right roads and the good. Today I say to you, we are the people, you and I. And it is time to let the people rule! Thank you. (ang.)
+	Postać: senator Charles Palantine
+
+  </section>
+<!--
+<script type="text/javascript">
+
+	(function(){
+		var documentEl = $(document),
+		parallaxBg = $('#img1');
+
+		documentEl.on('scroll', function(){
+			var currScrollPos = documentEl.scrollTop();
+			parallaxBg.css('background-position', '0 ' + -currScrollPos/4 + 'px');
+		});
+	})();
+</script>
+-->     
+<div id="footer">Designed by testow68@gmail.com. Wszystkie prawa zastrzeżone.</div>
+    </body>
+
+</html>
