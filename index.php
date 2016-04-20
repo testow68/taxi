44bd7c2c388afcbffb286b09da6b2f670faf13b1
Przemysław Piwowarski <prz.piw@mail.com>
2016-04-20 14:29:26 +0200

    bootstrap

--- index.php
+++ index.php
@@ -15,37 +15,205 @@ and open the template in the editor.
 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script type="text/javascript" src="http://ciasteczka.eu/cookiesEU-latest.min.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     </head>
-    <body>
-        
+  
+
+
+
+   <body data-spy="scroll" data-target="#my-navbar">
+
+  <!-- Navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <a href="" class="navbar-brand">TaxiDriver</a>
+        </div><!-- Navbar Header-->
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+          
+        <a href="" class="btn btn-warning navbar-btn navbar-right">Download Now</a>
+          <ul class="nav navbar-nav">
+            <li><a href="#feedback">Feedback</a> 
+            <li><a href="#gallery">Gallery</a> 
+            <li><a href="#features">Features</a> 
+            <li><a href="#faq">Faq</a> 
+            <li><a href="#contact">ContactUs</a> 
+          </ul>
+        </div>
+      </div><!-- End Container-->
+    </nav>
+          <div class="backgr">
           <div id="img1" class="parallax-bg">
     <h1>You talkin' to me?</h1>
     <a class="ghost" href="#">Download</a>
-  </div>
-        <header>
-           The idea had been growing in my brain <br>
-           for some time...
-        </header>
-  <section>
-  Ladies and gentlemen… we are met today at a crossroads, Columbus Circle. This is no ordinary place. It is a place where many roads and many lives intersect. It is appropriate that we meet here today, because these are not ordinary times. We meet at a crossroads in history. For far too long, the wrong roads have been taken. The wrong roads have led us into war, into poverty, into unemployment, and inflation. Today I say to you, we have reached the turning point. No longer will we, the people, suffer for the few. I would lie to you if I told you the new roads would be easy. They will not be easy. Nothing that is right and good has ever been easy. We, the people, know that. And we, the people, know the right roads and the good. Today I say to you, we are the people, you and I. And it is time to let the people rule! Thank you. (ang.)
-	Postać: senator Charles Palantine
-    
+  </div></div>
+  <div id="feedback">
+       <div class="jumbotron">
+      <div class="container text-center">
+        <h1>The idea had been growing in my brain <br>
+           for some time...</h1>
+        <p>Ladies and gentlemen… we are met today at a crossroads, Columbus Circle. This is no ordinary place. It is a place where many roads and many lives intersect. It is appropriate that we meet here today, because these are not ordinary times. We meet at a crossroads in history. For far too long, the wrong roads have been taken. The wrong roads have led us into war, into poverty, into unemployment, and inflation. Today I say to you, we have reached the turning point. No longer will we, the people, suffer for the few. I would lie to you if I told you the new roads would be easy. They will not be easy. Nothing that is right and good has ever been easy. We, the people, know that. And we, the people, know the right roads and the good. Today I say to you, we are the people, you and I. And it is time to let the people rule! Thank you. (ang.)
+  Postać: senator Charles Palantine
+     </p>
 
-  </section>
-      
-      <div id="img2" class="parallax-bg">
+        <div class="btn-group">
+          <a href="" class="btn btn-lg btn-warning">Download App</a>
+          <a href="" class="btn btn-lg btn-default">Visit store</a>
+          <a href="" class="btn btn-lg btn-warning">Spread the word</a>
+        </div>
+      </div><!-- End container -->
+    </div>
+  </div>
+
+
+
+
+
+<div class="container">
+    <section>
+      <div class="page-header" id="features">
+          <h2>Features.<small> Some of the coolest Features of this app.</small></h2>
+        </div><!-- End Page Header -->
+
+      <div class="row">
+          <div class="arti">
+            <h3>This is the heading</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elit</p>
+          </div>
+         
+        </div><!-- End row -->
+        <div class="row">
+           <div class="arti">
+            <h3>This is the heading</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elit</p>
+          </div>
+         
+        </div>
+        <div class="row">
+           <div class="arti">
+            <h3>This is the heading</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elit</p>
+          </div>
+         
+        </div>
+<br><br><br><br><br>
+
+       
+    </section>
+  </div>
+
+
+
+
+      <div class="backgr">
+      <div id="img3" class="parallax-bg">
     <h1>Then who the hell else <br> are you talking to?</h1>
-  </div>
-        
-         <header>
+  </div></div>
+    <div id="gallery">     
+          <div class="jumbotron">
+      <div class="container text-center">
+        <h1>
            Thank God for the rain, which washed away <br> the garbage and the trash off the sidewalks.
-        </header>
-  <section>
+        </h1>
+  <p>
   Ladies and gentlemen… we are met today at a crossroads, Columbus Circle. This is no ordinary place. It is a place where many roads and many lives intersect. It is appropriate that we meet here today, because these are not ordinary times. We meet at a crossroads in history. For far too long, the wrong roads have been taken. The wrong roads have led us into war, into poverty, into unemployment, and inflation. Today I say to you, we have reached the turning point. No longer will we, the people, suffer for the few. I would lie to you if I told you the new roads would be easy. They will not be easy. Nothing that is right and good has ever been easy. We, the people, know that. And we, the people, know the right roads and the good. Today I say to you, we are the people, you and I. And it is time to let the people rule! Thank you. (ang.)
 	Postać: senator Charles Palantine
 
-  </section>
-<!--
+  </p>
+    <div class="btn-group">
+          <a href="" class="btn btn-lg btn-warning">Download App</a>
+          <a href="" class="btn btn-lg btn-default">Visit store</a>
+          <a href="" class="btn btn-lg btn-warning">Spread the word</a>
+        </div>
+      </div><!-- End container -->
+    </div>
+  </div>
+
+<div class="container">
+      <section>
+        <div class="page-header" id="feedback">
+          <h2>Feedback.<small> Check out the Awesome Feedback</small></h2>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <blockquote>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida quam ac ante rutrum, in mollis ligula mattis. Integer nulla nisi, ullamcorper et posuere vel</p>
+              <footer>John doe</footer>
+            </blockquote>
+          </div>
+          <div class="col-md-4">
+            <blockquote>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida quam ac ante rutrum, in mollis ligula mattis. Integer nulla nisi, ullamcorper et posuere vel</p>
+              <footer>John doe</footer>
+            </blockquote>
+          </div>
+          <div class="col-md-4">
+            <blockquote>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida quam ac ante rutrum, in mollis ligula mattis. Integer nulla nisi, ullamcorper et posuere vel</p>
+              <footer>John doe</footer>
+            </blockquote>
+          </div>
+        </div><!-- End row -->
+      </section><br><br><br><br><br>
+
+    </div>
+
+ <div class="backgr">
+
+    <div id="img2" class="parallax-bg">
+    <h1> Well I'm the only one here. </h1>
+  </div></div>
+
+<div class="container">
+    <section>
+      <div class="page-header" id="features">
+          <h2>Features.<small> Some of the coolest Features of this app.</small></h2>
+        </div><!-- End Page Header -->
+
+      <div class="row">
+          <div class="arti">
+            <h3>This is the heading</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elit</p>
+          </div>
+         
+        </div><!-- End row -->
+        <div class="row">
+           <div class="arti">
+            <h3>This is the heading</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elit</p>
+          </div>
+         
+        </div>
+        <div class="row">
+           <div class="arti">
+            <h3>This is the heading</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elitLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris tortor, eleifend sit amet fringilla ac, tincidunt id massa. Proin et odio mattis, venenatis lacus vel, faucibus elit</p>
+          </div>
+         
+        </div>
+<br><br><br><br><br>
+
+       
+    </section>
+  </div>
+
+
+
+
+
+
+
+
+
+
 <script type="text/javascript">
 
 	(function(){
@@ -58,8 +226,43 @@ and open the template in the editor.
 		});
 	})();
 </script>
--->     
-<div id="footer">Designed by testow68@gmail.com. Wszystkie prawa zastrzeżone.</div>
+  
     </body>
+
+    <footer>
+      <hr>
+        <div class="container text-center">
+        <h3>Subscribe for more free stuff</h3>
+        <p>Enter your name and email</p>
+
+        <form action="" class="form-inline">
+          <div class="form-group">
+            <label for="subscription">Subscribe</label>
+            <input type="text" class="form-control" id="subscription" placeholder="Your name">
+          </div>
+          <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter your Email">
+          </div>
+          <button type="submit" class="btn btn-default">Subscribe</button>
+          
+        </form>
+
+        <hr>
+        <ul class="list-inline">
+          <li><a href="http://www.twitter.com/wiredwiki">Twitter</a></li>
+          <li><a href="http://www.facebook.com/askorama">Facebook</a></li>
+          <li><a href="http://www.youtube.com/wiredwiki">YouTube</a></li>
+        </ul>
+        
+        <p>&copy; Copyright @ 2016. Designed by testow68@gmail.com.</p>
+      </div><!-- end Container-->
+      
+
+    </footer>
+
+  <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 </html>
